da378c73494defafea898e299b0d890aba7dbc54
mikuta0407 <flute.takkun0407@gmail.com>
2019-09-07 18:41:12 +0900

    Upload

---
+++ v1.js
@@ -0,0 +1,56 @@
+function KokkaiAllChecked(){
+    var all = document.Kokkai.KokkaiAll.checked;
+    for (var i=0; i<document.Kokkai.kokkai.length; i++){
+      document.Kokkai.kokkai[i].checked = all;
+    }
+  }
+
+  // 一つでもチェックを外すと「全て選択」のチェック外れる
+function KokkaiDisChecked(){
+    var checks = document.Kokkai.kokkai;
+    var checksCount = 0;
+    
+    for (var i=0; i<checks.length; i++){
+        if (checks[i].checked == false) {
+            document.Kokkai.KokkaiAll.checked = false;
+        } else {
+        checksCount += 1;
+            if (checksCount == checks.length) {
+                document.Kokkai.KokkaiAll.checked = true;
+            }
+        }
+    }
+}
+
+function ChihouAllChecked(){
+    var all = document.Chihou.ChihouAll.checked;
+    for (var i=0; i<document.Chihou.chihou.length; i++){
+      document.Chihou.chihou[i].checked = all;
+    }
+  }
+
+  // 一つでもチェックを外すと「全て選択」のチェック外れる
+function ChihouDisChecked(){
+    var checks = document.Chihou.chihou;
+    var checksCount = 0;
+    
+    for (var i=0; i<checks.length; i++){
+        if (checks[i].checked == false) {
+            document.Chihou.ChihouAll.checked = false;
+        } else {
+        checksCount += 1;
+            if (checksCount == checks.length) {
+                document.Chihou.ChihouAll.checked = true;
+            }
+        }
+    }
+}
+
+function generate(){
+    var dateform = document.forms.date;
+    start = dateform.start.value;
+    end = dateform.end.value;
+    
+    var kdietform = document.forms.Kokkai;
+    var kokkaigikaimei
+}
